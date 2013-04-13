cca18e73fce67f13f413a0dae5066233a5394430
danginsburg <dginsburg@upsamplesoftware.com>
2013-04-13 11:12:15 -0400

    Add Vertex Array Object example.

---
+++ Chapter_6/VertexArrayObjects/VertexArrayObjects.c
@@ -0,0 +1,169 @@
+//
+// Book:      OpenGL(R) ES 2.0 Programming Guide
+// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
+// ISBN-10:   0321502795
+// ISBN-13:   9780321502797
+// Publisher: Addison-Wesley Professional
+// URLs:      http://safari.informit.com/9780321563835
+//            http://www.opengles-book.com
+//
+
+// VertexArraybjects.c
+//
+//    This example demonstrates drawing a primitive with
+//    Vertex Array Objects (VAOs)
+//
+#include "esUtil.h"
+
+typedef struct
+{
+   // Handle to a program object
+   GLuint programObject;
+
+   // VertexBufferObject Ids
+   GLuint vboIds[2];
+
+   // VertexArrayObject Id
+   GLuint vaoId;
+
+} UserData;
+
+
+#define VERTEX_POS_SIZE       3 // x, y and z
+#define VERTEX_COLOR_SIZE     4 // r, g, b, and a
+
+#define VERTEX_POS_INDX       0
+#define VERTEX_COLOR_INDX     1
+
+#define VERTEX_STRIDE         ( sizeof(GLfloat) *     \
+                               ( VERTEX_POS_SIZE +    \
+                                 VERTEX_COLOR_SIZE ) )
+
+
+int Init ( ESContext *esContext )
+{
+   UserData *userData = (UserData*) esContext->userData;
+   const char vShaderStr[] =  
+      "#version 300 es                            \n"
+      "layout(location = 0) in vec4 a_position;   \n"
+      "layout(location = 1) in vec4 a_color;      \n"
+      "out vec4 v_color;                          \n"
+      "void main()                                \n"
+      "{                                          \n"
+      "    v_color = a_color;                     \n"
+      "    gl_Position = a_position;              \n"
+      "}";
+
+   
+   const char fShaderStr[] =
+      "#version 300 es            \n"
+      "precision mediump float;   \n"
+      "in vec4 v_color;           \n"
+      "out vec4 o_fragColor;      \n"
+      "void main()                \n"
+      "{                          \n"
+      "    o_fragColor = v_color; \n"
+      "}" ;
+
+   GLuint programObject;
+   
+   // 3 vertices, with (x,y,z) ,(r, g, b, a) per-vertex
+   GLfloat vertices[3 * (VERTEX_POS_SIZE + VERTEX_COLOR_SIZE)] = 
+   {  
+       0.0f,  0.5f, 0.0f,        // v0
+       1.0f,  0.0f, 0.0f, 1.0f,  // c0
+      -0.5f, -0.5f, 0.0f,        // v1
+       0.0f,  1.0f, 0.0f, 1.0f,  // c1
+       0.5f, -0.5f, 0.0f,        // v2
+       0.0f,  0.0f, 1.0f, 1.0f,  // c2
+   };
+   // Index buffer data
+   GLushort indices[3] = { 0, 1, 2 };
+
+   // Create the program object
+   programObject = esLoadProgram ( vShaderStr, fShaderStr );
+   
+   if ( programObject == 0 )
+      return GL_FALSE;
+
+   // Store the program object
+   userData->programObject = programObject;
+
+   // Generate VBO Ids and load the VBOs with data
+   glGenBuffers ( 2, userData->vboIds );
+
+   glBindBuffer ( GL_ARRAY_BUFFER, userData->vboIds[0] );
+   glBufferData ( GL_ARRAY_BUFFER, sizeof(vertices),
+                  vertices, GL_STATIC_DRAW);
+   glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, userData->vboIds[1]); 
+   glBufferData ( GL_ELEMENT_ARRAY_BUFFER, sizeof ( indices ),
+                  indices, GL_STATIC_DRAW );
+
+   // Generate VAO Id
+   glGenVertexArrays ( 1, &userData->vaoId );
+
+   // Bind the VAO and then setup the vertex
+   // attributes
+   glBindVertexArray ( userData->vaoId );
+
+   glBindBuffer(GL_ARRAY_BUFFER, userData->vboIds[0]);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, userData->vboIds[1]); 
+
+   glEnableVertexAttribArray(VERTEX_POS_INDX); 
+   glEnableVertexAttribArray(VERTEX_COLOR_INDX); 
+
+   glVertexAttribPointer ( VERTEX_POS_INDX, VERTEX_POS_SIZE,
+      GL_FLOAT, GL_FALSE, VERTEX_STRIDE, (const void*) 0 );
+  
+   glVertexAttribPointer ( VERTEX_COLOR_INDX, VERTEX_COLOR_SIZE,
+      GL_FLOAT, GL_FALSE, VERTEX_STRIDE,
+      (const void*) ( VERTEX_POS_SIZE * sizeof(GLfloat) ) );
+
+   // Reset to the default VAO
+   glBindVertexArray ( 0 );
+
+   glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
+   return GL_TRUE;
+}
+
+void Draw ( ESContext *esContext )
+{
+   UserData *userData = (UserData*) esContext->userData;
+      
+   glViewport ( 0, 0, esContext->width, esContext->height );   
+   glClear ( GL_COLOR_BUFFER_BIT );
+   glUseProgram ( userData->programObject );
+   
+   // Bind the VAO 
+   glBindVertexArray ( userData->vaoId );
+
+   // Draw with the VAO settings
+   glDrawElements ( GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, (const void*) 0 );
+   
+   // Return to the default VAO
+   glBindVertexArray ( 0 );
+}
+
+void Shutdown ( ESContext *esContext )
+{
+   UserData *userData = (UserData*) esContext->userData;
+
+   glDeleteProgram ( userData->programObject );
+   glDeleteBuffers ( 2, userData->vboIds );
+   glDeleteVertexArrays( 1, &userData->vaoId );
+}
+
+int esMain( ESContext *esContext )
+{
+   esContext->userData = malloc ( sizeof( UserData ) );
+
+   esCreateWindow ( esContext, "VertexArrayObjects", 320, 240, ES_WINDOW_RGB );
+   
+   if ( !Init ( esContext ) )
+      return GL_FALSE;
+
+   esRegisterShutdownFunc ( esContext, Shutdown );
+   esRegisterDrawFunc ( esContext, Draw );
+   
+   return GL_TRUE;
+}
