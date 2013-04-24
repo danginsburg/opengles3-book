af1ada2029f7df5e68f26bafb0e8218a91fb1d71
danginsburg <dginsburg@upsamplesoftware.com>
2013-04-24 10:56:15 -0400

    Update Simple_VertexShader to match closer to text.

--- Chapter_8/Simple_VertexShader/Simple_VertexShader.c
+++ Chapter_8/Simple_VertexShader/Simple_VertexShader.c
@@ -46,18 +46,22 @@ int Init ( ESContext *esContext )
       "#version 300 es                             \n"
       "uniform mat4 u_mvpMatrix;                   \n"
       "layout(location = 0) in vec4 a_position;    \n"
+      "layout(location = 1) in vec4 a_color;       \n"
+      "out vec4 v_color;                           \n"
       "void main()                                 \n"
       "{                                           \n"
+      "   v_color = a_color;                       \n"
       "   gl_Position = u_mvpMatrix * a_position;  \n"
       "}                                           \n";
    
    const char fShaderStr[] =  
       "#version 300 es                                \n"
       "precision mediump float;                       \n"
+      "in vec4 v_color;                               \n"
       "layout(location = 0) out vec4 outColor;        \n"
       "void main()                                    \n"
       "{                                              \n"
-      "  outColor = vec4( 1.0, 0.0, 0.0, 1.0 );       \n"
+      "  outColor = v_color;                          \n"
       "}                                              \n";
 
    // Load the shaders and get a linked program object
@@ -133,8 +137,11 @@ void Draw ( ESContext *esContext )
    // Load the vertex position
    glVertexAttribPointer ( 0, 3, GL_FLOAT, 
                            GL_FALSE, 3 * sizeof(GLfloat), userData->vertices );
-   
+
    glEnableVertexAttribArray ( 0 );
+
+   // Set the vertex color to red
+   glVertexAttrib4f( 1, 1.0f, 0.0f, 0.0f, 1.0f );
    
    // Load the MVP matrix
    glUniformMatrix4fv( userData->mvpLoc, 1, GL_FALSE, (GLfloat*) &userData->mvpMatrix.m[0][0] );
