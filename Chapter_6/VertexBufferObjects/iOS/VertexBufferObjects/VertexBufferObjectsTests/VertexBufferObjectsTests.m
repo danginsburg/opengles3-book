44e7e3442696582aeec3c42529b8a85e2beee799
Dan Ginsburg <dginsburg@upsamplesoftware.com>
2013-09-23 21:11:22 -0400

    Add iOS7 VertexArrayObjects and VertexBufferObjects

---
+++ Chapter_6/VertexBufferObjects/iOS/VertexBufferObjects/VertexBufferObjectsTests/VertexBufferObjectsTests.m
@@ -0,0 +1,34 @@
+//
+//  VertexBufferObjectsTests.m
+//  VertexBufferObjectsTests
+//
+//  Created by Daniel Ginsburg on 9/23/13.
+//  Copyright (c) 2013 Daniel Ginsburg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VertexBufferObjectsTests : XCTestCase
+
+@end
+
+@implementation VertexBufferObjectsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
