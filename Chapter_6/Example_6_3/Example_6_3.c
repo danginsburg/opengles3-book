abdbd7c19e20763354df1e2e22ee1b27ddf0bb4b
Dan Ginsburg <dginsburg@upsamplesoftware.com>
2013-10-25 16:41:03 -0400

    Update clear color of Example 6-3 to white.

--- Chapter_6/Example_6_3/Example_6_3.c
+++ Chapter_6/Example_6_3/Example_6_3.c
@@ -85,7 +85,7 @@ int Init ( ESContext *esContext )
    // Store the program object
    userData->programObject = programObject;
 
-   glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
+   glClearColor ( 1.0f, 1.0f, 1.0f, 0.0f );
    return GL_TRUE;
 }
 
