576650659be71819cdf02e1f883b3328b31ca065
Dan Ginsburg <dginsburg@upsamplesoftware.com>
2013-11-19 18:27:00 -0500

    Add iOS MRTs project.

---
+++ Chapter_11/MRTs/iOS/MRTs/MRTsTests/MRTsTests.m
@@ -0,0 +1,34 @@
+//
+//  MRTsTests.m
+//  MRTsTests
+//
+//  Created by Daniel Ginsburg on 11/19/13.
+//  Copyright (c) 2013 Daniel Ginsburg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MRTsTests : XCTestCase
+
+@end
+
+@implementation MRTsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
